2e16b0b7b44277a5db7ab360864a88ee2b71fc78
Moritz Sichert <moritz.sichert@googlemail.com>
2016-06-13 19:30:47 +0200

    Improved Register

--- include/register.hpp
+++ include/register.hpp
@@ -12,33 +12,30 @@ class Register {
 private:
     friend class RegisterManager;
 
-    char value[8];
+    alignas(8) char value[8];
 
     Register() {}
 
 public:
-    // integral types or any other type with 8 bytes size and standard layout
+    // any type with at most 8 bytes size and standard layout
     template <typename T>
     inline typename std::enable_if<
-        std::is_integral<T>::value || (
-            sizeof(T) == 8 &&
-            std::is_standard_layout<T>::value
-        ),
+        sizeof(T) <= 8 &&
+        std::is_standard_layout<T>::value,
         T
     >::type get() const {
         T retval;
-        memcpy(&retval, value, 8);
+        memcpy(&retval, value, sizeof(T));
         return retval;
     }
 
     template <typename T>
     inline typename std::enable_if<
-        std::is_integral<T>::value || (
-            sizeof(T) == 8 &&
-            std::is_standard_layout<T>::value
-        )
+        sizeof(T) <= 8 &&
+        std::is_standard_layout<T>::value
     >::type set(const T& new_value) {
-        memcpy(value, &new_value, 8);
+        memcpy(value, &new_value, sizeof(T));
+        memset(value + sizeof(T), 0, 8 - sizeof(T));
     }
 
     static inline Register from_bytes(const char* value) {
@@ -47,18 +44,14 @@ public:
         return r;
     }
 
-    bool operator==(const Register& reg) const { 
-        for (int i = 0; i < 8; i++) {
-            if (reg.value[i] != value[i])
-                return false;
-        }
-        return true;
+    bool operator==(const Register& reg) const {
+        return memcmp(value, reg.value, 8) == 0;
     }
 
-    unsigned getHash() const {
-        uint64_t typvalue;
-        memcpy(&typvalue, value, 8);
-        return typvalue;
+    size_t getHash() const {
+        constexpr uint64_t prime = (1ull << 63) - 471;
+        uint64_t typvalue = get<uint64_t>();
+        return typvalue * prime;
     }
 
 };
@@ -67,6 +60,6 @@ public:
 namespace std {
 template <>
 struct hash<IN2118::Register> {
-    unsigned operator()(const IN2118::Register& r) const { return r.getHash(); };
+    size_t operator()(const IN2118::Register& r) const { return r.getHash(); };
 };
 };
